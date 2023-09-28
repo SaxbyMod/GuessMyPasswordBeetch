477546c404ed2d1bead4ce4d5f7331418a2f560b
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2023-09-27 21:05:40 -0400

    Initial commit

---
+++ GuessMyPasswordBeetch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyPasswordBeetch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (bool i = false; i != true; i = i) {
+                Console.WriteLine("GUESS MY PASSWORD BEETCH");
+                string items = "ALPHA";
+                Console.ReadLine();
+                string Awnser;
+                Console.WriteLine("ENTER PASSWORD:");
+                Awnser = Console.ReadLine();
+                if (Awnser == items)
+                {
+                    Console.WriteLine("GG");
+                    i = true;
+                } else
+                {
+                    Console.WriteLine("Back to step 1");
+                    i = false;
+                }
+            }
+        }
+    }
+}
